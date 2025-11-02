6ebf99194027dd27cc16989f176424cfe61ce67a
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-11-02 14:57:46 -0800

    function as prop

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,20 +1,27 @@
-function Button({ text = "Click me!", color = "blue", fontSize = 12 }) {
+function Button({ text = "Click me!", color = "blue", fontSize = 12, handleClick }) {
     const buttonStyle = {
         color: color,
         fontSize: fontSize + 'px'
     };
 
     return (
-        <button style={buttonStyle}>{text}</button>
+        <button onClick={handleClick} style={buttonStyle}>
+            {text}
+        </button>
     );
 }
 
 export default function MultipleButtons() {
+    const handleButtonClick = () => {
+        window.location.href = "https://www.google.com";
+    };
+
     return (
         <div>
             <Button />
             <Button text="Don't Click me!" color="red" />
             <Button fontSize={20} />
+            <Button handleClick={handleButtonClick} />
         </div>
     );
 }
