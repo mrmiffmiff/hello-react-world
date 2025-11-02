ca939d7a40de0309074d6d64247e13d957b7c2ef
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-11-01 19:58:17 -0700

    better greeting

---
+++ src/Greeting.jsx
@@ -0,0 +1,8 @@
+function ImportantGreeting() {
+    return <div>
+        <h1>&quot;I'm late, I'm late, for a very important date.&quot;</h1>
+        <p>And that's why you must always watch the time.</p>
+    </div>
+}
+
+export default ImportantGreeting;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AppHello from './AppHello.jsx'
+import ImportantGreeting from './Greeting.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <AppHello />
+    {/* <AppHello /> */}
+    <ImportantGreeting />
   </StrictMode>,
 )
