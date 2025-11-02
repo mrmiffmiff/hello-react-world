2f89b72bbf3da67704981bc7b481fc5f7eda899b
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-11-01 20:42:35 -0700

    wrap returns in parentheses

--- src/FavoriteFood.jsx
+++ src/FavoriteFood.jsx
@@ -1,21 +1,25 @@
 import noodles from '/public/noodles-and-cottage-cheese.jpg';
 
 function GreatEasyMeal() {
-    return <div>
-        <img src={noodles} alt="Noodles and Cottage Cheese" />
-        <p>I&#39;m personally rather fond of this basic dish. Cook some noodles, heap on some cottage cheese, warm it more if you wish, and dig in.</p>
-    </div>
+    return (
+        <div>
+            <img src={noodles} alt="Noodles and Cottage Cheese" />
+            <p>I&#39;m personally rather fond of this basic dish. Cook some noodles, heap on some cottage cheese, warm it more if you wish, and dig in.</p>
+        </div>
+    )
 }
 
 function AnotherGoodMeal() {
-    return <>
-        <h2>
-            I also suggest lentil stews in slow cookers.
-        </h2>
-        <h3>
-            Slow cookers are great in general.
-        </h3>
-    </>
+    return (
+        <>
+            <h2>
+                I also suggest lentil stews in slow cookers.
+            </h2>
+            <h3>
+                Slow cookers are great in general.
+            </h3>
+        </>
+    )
 }
 
 export { GreatEasyMeal, AnotherGoodMeal }
--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,8 +1,10 @@
 function ImportantGreeting() {
-    return <div>
-        <h1>&quot;I'm late, I'm late, for a very important date.&quot;</h1>
-        <p>And that's why you must always watch the time.</p>
-    </div>
+    return (
+        <div>
+            <h1>&quot;I'm late, I'm late, for a very important date.&quot;</h1>
+            <p>And that's why you must always watch the time.</p>
+        </div>
+    )
 }
 
 export default ImportantGreeting;
